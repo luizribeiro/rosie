1514c6876c0355caa456d7362449e551b29c9d6c
Luiz Ribeiro <luizribeiro@gmail.com>
2023-11-25 16:48:34 -0500

    make tools async too

--- tools.py
+++ tools.py
@@ -2,10 +2,10 @@ from langchain.agents import tool
 
 
 @tool
-def control_house_appliances(query: str) -> str:
+async def control_house_appliances(query: str) -> str:
     """Controls house appliances."""
     print(f"control_house_appliances: {query}")
-    return "Done"
+    return "Error: Unknown appliance."
 
 
 TOOLS = [
