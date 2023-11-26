b05801eb69b93f0079264cf35c8c47767acfc0f6
Luiz Ribeiro <luizribeiro@gmail.com>
2023-11-26 16:47:52 -0500

    added llama2 model

--- src/models.py
+++ src/models.py
@@ -5,4 +5,5 @@ LLMS = {
     "gpt-3.5": lambda: ChatOpenAI(model_name="gpt-3.5-turbo-1106"),
     "gpt-4": lambda: ChatOpenAI(model_name="gpt-4-1106-preview"),
     "mistral": lambda: ChatOllama(model="mistral:7b-instruct-q5_1"),
+    "llama2": lambda: ChatOllama(model="llama2:13b-chat-q4_0"),
 }
