654caa72d004f2a6541f3509a1af174648fdb6df
Luiz Ribeiro <luizribeiro@gmail.com>
2023-11-26 10:39:36 -0500

    enabled mistral as a model through ollama

--- devenv.nix
+++ devenv.nix
@@ -3,6 +3,12 @@
 {
   packages = with pkgs; [
     git
+    (ollama.override {
+      llama-cpp = (llama-cpp.override {
+        cudaSupport = true;
+        openblasSupport = false;
+      });
+    })
     (python310.withPackages (p: [
       p.fastapi
       p.langchain
--- src/config.py
+++ src/config.py
@@ -1,7 +1,8 @@
-from langchain.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOllama, ChatOpenAI
 
 
 LLMS = {
     "gpt-3.5": lambda: ChatOpenAI(model_name="gpt-3.5-turbo-1106"),
     "gpt-4": lambda: ChatOpenAI(model_name="gpt-4-1106-preview"),
+    "mistral": lambda: ChatOllama(model="mistral"),
 }
