80e34148fe28b00b7e26ce964b01b4e1b4776770
Luiz Ribeiro <luizribeiro@gmail.com>
2023-11-26 11:23:57 -0500

    changed default mistral model

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ from langchain.chat_models import ChatOllama, ChatOpenAI
 LLMS = {
     "gpt-3.5": lambda: ChatOpenAI(model_name="gpt-3.5-turbo-1106"),
     "gpt-4": lambda: ChatOpenAI(model_name="gpt-4-1106-preview"),
-    "mistral": lambda: ChatOllama(model="mistral"),
+    "mistral": lambda: ChatOllama(model="mistral:7b-instruct-q5_1"),
 }
