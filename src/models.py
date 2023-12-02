db8c84918fd3dbbf70cf1290ae6d7ba57404397f
Luiz Ribeiro <luizribeiro@gmail.com>
2023-12-02 12:53:32 -0500

    updated available models

--- src/models.py
+++ src/models.py
@@ -4,6 +4,7 @@ from langchain.chat_models import ChatOllama, ChatOpenAI
 LLMS = {
     "gpt-3.5": lambda: ChatOpenAI(model_name="gpt-3.5-turbo-1106"),
     "gpt-4": lambda: ChatOpenAI(model_name="gpt-4-1106-preview"),
-    "mistral": lambda: ChatOllama(model="mistral:7b-instruct-q5_1"),
+    "mistral": lambda: ChatOllama(model="mistral:7b-instruct-q6_K"),
     "llama2": lambda: ChatOllama(model="llama2:13b-chat-q4_0"),
+    "starling": lambda: ChatOllama(model="starling-lm:7b-alpha-q8_0"),
 }
